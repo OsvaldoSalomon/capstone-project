e1f3bef2cd9f927646950138ef8fa152defa3234
Osvaldo Salomon <osvaldo.salo21@gmail.com>
2022-07-17 13:45:28 -0400

    Fixed broken optional profile pic

--- react-app/src/components/Profile/ProfileCard/index.js
+++ react-app/src/components/Profile/ProfileCard/index.js
@@ -1,11 +1,15 @@
 import './ProfileCard.css'
 
 const ProfileCard = ({ user }) => {
+    const addDefaultSrc = (ev) => {
+        ev.target.src = 'https://images.unsplash.com/photo-1641423914598-288fee6cecf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+    }
+
     return (
         <div className='profileCardBody'>
             <div className='profileCardBackground'></div>
             <div className='profileCardInfo'>
-                <img className="profileImageLogo" src={user.profilePic} alt={user.username} />
+                <img onError={addDefaultSrc} className="profileImageLogo" src={user.profilePic} alt={user.username} />
                 <h3>{user?.firstName} {user?.lastName}</h3>
                 <h4>@{user?.username}</h4>
                 <p className='profileBio'>"{user?.bio}"</p>
