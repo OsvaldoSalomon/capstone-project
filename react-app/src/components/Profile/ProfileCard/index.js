fac19fcce9ddc9134a78a5674420e7b65cff8564
Osvaldo Salomon <osvaldo.salo21@gmail.com>
2022-07-20 11:08:20 -0400

    Updated styles and fixed the broken image

--- react-app/src/components/Profile/ProfileCard/index.js
+++ react-app/src/components/Profile/ProfileCard/index.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import './ProfileCard.css'
 
 const ProfileCard = ({ user }) => {
-    const sessionUser = useSelector((state) => state.session.user);
+    // const sessionUser = useSelector((state) => state.session.user);
 
     const addDefaultSrc = (ev) => {
         ev.target.src = 'https://images.unsplash.com/photo-1641423914598-288fee6cecf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
